18541527285fa4022c414e76bd63308c295c3e8e
Matheus Silva <41930738+Matheusobruxo@users.noreply.github.com>
2023-07-12 17:14:17 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace PSAula03_Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.panelSaldo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.chkAumentar = new System.Windows.Forms.CheckBox();
+            this.txtAumentar = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.panelSaldo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold);
+            this.lblUser.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblUser.Location = new System.Drawing.Point(12, 18);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(176, 27);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Digite seu usuário";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold);
+            this.lblLogin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblLogin.Location = new System.Drawing.Point(12, 67);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(164, 27);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "Digite sua senha";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(221, 16);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 34);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(221, 64);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(100, 34);
+            this.txtPass.TabIndex = 3;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnConectar.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnConectar.Location = new System.Drawing.Point(88, 119);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(178, 64);
+            this.btnConectar.TabIndex = 4;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = false;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // panelSaldo
+            // 
+            this.panelSaldo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelSaldo.Controls.Add(this.btnSalvar);
+            this.panelSaldo.Controls.Add(this.txtAumentar);
+            this.panelSaldo.Controls.Add(this.chkAumentar);
+            this.panelSaldo.Controls.Add(this.lblSaldo);
+            this.panelSaldo.Controls.Add(this.label1);
+            this.panelSaldo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panelSaldo.Location = new System.Drawing.Point(398, 18);
+            this.panelSaldo.Name = "panelSaldo";
+            this.panelSaldo.Size = new System.Drawing.Size(288, 170);
+            this.panelSaldo.TabIndex = 5;
+            this.panelSaldo.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(16, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Seu saldo: R$";
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSaldo.Location = new System.Drawing.Point(140, 5);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(31, 23);
+            this.lblSaldo.TabIndex = 7;
+            this.lblSaldo.Text = "R$";
+            // 
+            // chkAumentar
+            // 
+            this.chkAumentar.AutoSize = true;
+            this.chkAumentar.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkAumentar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.chkAumentar.Location = new System.Drawing.Point(21, 31);
+            this.chkAumentar.Name = "chkAumentar";
+            this.chkAumentar.Size = new System.Drawing.Size(103, 27);
+            this.chkAumentar.TabIndex = 8;
+            this.chkAumentar.Text = "Aumentar";
+            this.chkAumentar.UseVisualStyleBackColor = true;
+            // 
+            // txtAumentar
+            // 
+            this.txtAumentar.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAumentar.Location = new System.Drawing.Point(20, 64);
+            this.txtAumentar.Name = "txtAumentar";
+            this.txtAumentar.PasswordChar = '*';
+            this.txtAumentar.Size = new System.Drawing.Size(120, 30);
+            this.txtAumentar.TabIndex = 6;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.LightCoral;
+            this.btnSalvar.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSalvar.Location = new System.Drawing.Point(146, 64);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(106, 34);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(710, 237);
+            this.Controls.Add(this.panelSaldo);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblUser);
+            this.Name = "Form1";
+            this.Text = "Login ";
+            this.panelSaldo.ResumeLayout(false);
+            this.panelSaldo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Panel panelSaldo;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtAumentar;
+        private System.Windows.Forms.CheckBox chkAumentar;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSAula03_Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            string usuario= txtUser.Text;
+            string senha = txtPass.Text;
+
+            if (usuario== "Conradito" && senha == "123")
+            {
+                MessageBox.Show($"Logado com sucesso no usuário: {usuario} !!");
+                panelSaldo.Visible = true;
+
+            }
+            else if (usuario == "" || senha == "")
+            {
+                MessageBox.Show("Digita alguma coisa véinho !!");
+            }
+            else
+            {
+                MessageBox.Show("Usuário ou senha incorretos !!");
+                txtPass.Clear();
+                txtUser.Clear();
+            }
+
+
+        }
+    }
+}
